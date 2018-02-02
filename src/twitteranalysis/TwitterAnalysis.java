37471cf9364789a23674c2cad7303fc6394a453d
fraseryates1994 <fraseryates1994@gmail.com>
2018-02-02 15:44:34 +0000

    Can read tweets and associated comments

--- src/twitteranalysis/TwitterAnalysis.java
+++ src/twitteranalysis/TwitterAnalysis.java
@@ -1,6 +1,7 @@
 package twitteranalysis;
 
 import java.util.ArrayList;
+import java.util.List;
 import twitter4j.*;
 import twitter4j.conf.*;
 
@@ -22,7 +23,7 @@ public class TwitterAnalysis {
 
         int pageno = 1;
         String user = "cnn";
-        ArrayList statuses = new ArrayList();
+        ArrayList<Status> statuses = new ArrayList();
 
         while (true) {
             try {
@@ -33,12 +34,72 @@ public class TwitterAnalysis {
                     break;
                 }
             } catch (TwitterException e) {
-
                 e.printStackTrace();
             }
         }
-        System.out.println("Total: " + statuses.size());
+        System.out.println("Total Statuses: " + statuses.size());
+
+        // Print first status
+        System.out.println("OG Status: "+ statuses.get(0).getText() +"\n============================================");
+        
+        // Get replies
+        ArrayList<Status> replies = getDiscussion(statuses.get(0), twitter);
+
+        // Print replies
+        for (int i = 0; i < replies.size(); i++) {
+            System.out.print(replies.get(i).getUser().getName());
+            System.out.print(" - " + replies.get(i).getText());
+            System.out.print("\n");
+        }
     }
 
-}
+    static public ArrayList<Status> getDiscussion(Status status, Twitter twitter) {
+        ArrayList<Status> replies = new ArrayList<>();
+        ArrayList<Status> all = null;
 
+        try {
+            long id = status.getId();
+            String screenname = status.getUser().getScreenName();
+
+            Query query = new Query("@" + screenname + " since_id:" + id);
+
+            try {
+                query.setCount(100);
+            } catch (Throwable e) {
+                // enlarge buffer error?
+                query.setCount(30);
+            }
+
+            QueryResult result = twitter.search(query);
+            all = new ArrayList<Status>();
+
+            do {
+                List<Status> tweets = result.getTweets();
+
+                for (Status tweet : tweets) {
+                    if (tweet.getInReplyToStatusId() == id) {
+                        all.add(tweet);
+                    }
+                }
+
+                if (all.size() > 0) {
+                    for (int i = all.size() - 1; i >= 0; i--) {
+                        replies.add(all.get(i));
+                    }
+                    all.clear();
+                }
+
+                query = result.nextQuery();
+
+                if (query != null) {
+                    result = twitter.search(query);
+                }
+
+            } while (query != null);
+
+        } catch (Exception | OutOfMemoryError e) {
+            e.printStackTrace();
+        }
+        return replies;
+    }
+}
