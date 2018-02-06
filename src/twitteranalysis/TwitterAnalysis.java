6f095e07b6e491e0544cd8f7d644af6605473b51
fraseryates1994 <fraseryates1994@gmail.com>
2018-02-06 16:50:39 +0000

    evaluating swear words

--- src/twitteranalysis/TwitterAnalysis.java
+++ src/twitteranalysis/TwitterAnalysis.java
@@ -14,29 +14,31 @@ import twitter4j.Status;
 public class TwitterAnalysis {
 
     public static void main(String[] args) {
-        hasSwearWord();
         TwitterWrapper tw = new TwitterWrapper();
-        String user = "KimKardashian";
+        String user = "KhloeKardashian";
+        
+        System.out.println("Getting Statuses for... " + user);
         ArrayList<Status> statuses = tw.getStatuses(user);
-
         System.out.println("Total Statuses: " + statuses.size());
 
         // Print first status
-        System.out.println("OG Status: " + statuses.get(0).getText() + "\n============================================");
-        System.out.println("OG Status: ");
-
+        System.out.println("OG Status: " + statuses.get(3).getText() + "\n============================================");
+        
         // Get replies
-        ArrayList<Status> replies = tw.getDiscussion(statuses.get(0));
-
+        System.out.println("Getting replies for most recent status...");
+        ArrayList<Status> replies = tw.getDiscussion(statuses.get(3));
+        System.out.println("Total Replies: " + replies.size());
+              
         // Print replies
         for (int i = 0; i < replies.size(); i++) {
+            System.out.println("ID - " + replies.get(i).getUser().getId());
+            System.out.println("Name - " + replies.get(i).getUser().getName());
+            System.out.println("Screen Name - " + replies.get(i).getUser().getScreenName());
             System.out.println("Comment - " + replies.get(i).getText());
+            System.out.println("hasSwear? - " + hasSwearWord(replies.get(i)));
             System.out.println("TimeZone - " + replies.get(i).getUser().getTimeZone());
             System.out.println("Lang - " + replies.get(i).getUser().getLang());
             System.out.println("Created at - " + replies.get(i).getUser().getCreatedAt());
-            System.out.println("Name - " + replies.get(i).getUser().getName());
-            System.out.println("Screen Name - " + replies.get(i).getUser().getScreenName());
-            System.out.println("ID - " + replies.get(i).getUser().getId());
             System.out.println("favouriteCount - " + replies.get(i).getFavoriteCount());
             System.out.println("followerscount - " + replies.get(i).getUser().getFollowersCount());
             System.out.println("friends count - " + replies.get(i).getUser().getFriendsCount());
@@ -47,9 +49,17 @@ public class TwitterAnalysis {
         }
     }
 
-    public static void hasSwearWord() {
+    public static boolean hasSwearWord(Status reply) {
         JDBCWrapper wr = new JDBCWrapper("org.apache.derby.jdbc.ClientDriver","jdbc:derby://localhost:1527/SocialMedia", "social", "fraz");
         SocialMediaDB db = new SocialMediaDB(wr);
-        System.out.println(db.getAllSwears());
+        ArrayList<String> swears = new ArrayList();
+        swears.addAll(db.getAllSwears());
+        
+        for (int i = 0; i < swears.size(); i ++) {
+            if (reply.getText().contains(swears.get(i))) {
+                return true;
+            }
+        }
+        return false;
     }
 }
